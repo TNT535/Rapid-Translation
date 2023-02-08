632103a4b4c0b6be420c1b3bbfe4ed916d3eb761
TNT535 <62098728+TNT535@users.noreply.github.com>
2023-02-09 02:52:59 +0700

    Matching information to UserInfoUC, create chart view sample.

---
+++ RapidTranslationWPF_MVVM/Models/DayCountSearch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RapidTranslationWPF_MVVM.Models
+{
+    public class DayCountSearch
+    {
+        public string Day { get; set; }
+        public int Count { get; set; }
+    }
+}
